80128cf257a454af33b38ded9cc84cc6ccdcea30
LapMarcos <78131374+MarcosLaporte@users.noreply.github.com>
2024-11-27 19:05:38 -0300

    ADD: added login tests

---
+++ src/main/java/web/automation/gui/login/pages/SignInPage.java
@@ -0,0 +1,30 @@
+package web.automation.gui.login.pages;
+
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
+import com.zebrunner.carina.webdriver.decorator.PageOpeningStrategy;
+import com.zebrunner.carina.webdriver.gui.AbstractPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+public class SignInPage extends AbstractPage {
+    @FindBy(xpath = "//*[@id=\"login\"]/h2")
+    private ExtendedWebElement testTitle;
+
+    public SignInPage(WebDriver driver) {
+        super(driver);
+        setPageOpeningStrategy(PageOpeningStrategy.BY_ELEMENT);
+        setUiLoadedMarker(testTitle);
+    }
+
+    @FindBy(xpath = "//*[@id=\"username\"]")
+    public ExtendedWebElement userInput;
+
+    @FindBy(xpath = "//*[@id=\"password\"]")
+    public ExtendedWebElement passInput;
+
+    @FindBy(xpath = "//*[@id=\"submit\"]")
+    public ExtendedWebElement submitBtn;
+
+    @FindBy(xpath = "//*[@id=\"error\"]")
+    public ExtendedWebElement errorMsg;
+}
---
+++ src/main/java/web/automation/gui/login/pages/SuccessfulLoginPage.java
@@ -0,0 +1,23 @@
+package web.automation.gui.login.pages;
+
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
+import com.zebrunner.carina.webdriver.decorator.PageOpeningStrategy;
+import com.zebrunner.carina.webdriver.gui.AbstractPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+public class SuccessfulLoginPage extends AbstractPage {
+
+    @FindBy(xpath = "//*[@id=\"loop-container\"]/div/article/div[1]/h1")
+    public ExtendedWebElement successTitle;
+
+    public SuccessfulLoginPage(WebDriver driver) {
+        super(driver);
+        setPageOpeningStrategy(PageOpeningStrategy.BY_ELEMENT);
+        setUiLoadedMarker(successTitle);
+    }
+
+    @FindBy(xpath = "//*[@id=\"loop-container\"]/div/article/div[2]/div/div/div/a")
+    public ExtendedWebElement logoutBtn;
+
+}
